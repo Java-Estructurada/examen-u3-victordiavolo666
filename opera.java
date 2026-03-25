696a4d75c28994a2a14ac550e2605505bdb72009
Victor <azulcrema0789@gmail.com>
2026-03-25 08:14:42 -0600

    .

---
+++ opera.java
@@ -0,0 +1,8 @@
+public class opera{
+public static double cIVA(double precio){
+    double iv=precio*0.16;
+    double iba=iv+precio;
+    return iba;
+}
+}
+
--- solucionU3.java
+++ solucionU3.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
+public class solucionU3{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int opc=0;
+        /*double IBA=opera.cIVA(opc);  MANDAR A LLAMAR A LA FUNCIÓN
+        System.out.print(IBA);*/ 
+        do {
+            System.out.println("===SISTEMA DE AUDITORÍA TECH===");
+            System.out.println("1. Auditar facturas del día");
+            System.out.println("2. Calcular impuestos (IVA)");
+            System.out.println("3. Cerrar turno");
+            System.out.print("Selecciona una operación:");
+            opc=sc.nextInt();
+            
+            } while (opc!=3);
 
+        }
+}
